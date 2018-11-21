fc072aa7a08fb8763e889d946ad7797ea137bf5c
Dean Pemberton <dean@deanpemberton.com>
2018-11-22 09:27:01 +1300

    Install a different version of pyzmail depending on the version of python

--- setup.py
+++ setup.py
@@ -33,6 +33,24 @@ cmds = [
     'csirtg-mail=csirtg_mail.client:main'
 ]
 
+
+def get_requirements_by_version():
+    base_packages = [
+        'beautifulsoup4',
+        'chardet',
+        'lxml',
+        'html5lib'
+    ]
+
+    packages27 = ['pyzmail']
+    packages36 = ['pyzmail36']
+
+    if sys.version_info.major >= (3) and sys.version_info.minor >= (6):
+        return base_packages + packages36
+    else:
+        return base_packages + packages27
+
+
 setup(
     name="csirtg_mail",
     version=versioneer.get_version(),
@@ -42,23 +60,17 @@ setup(
     url="https://github.com/csirtgadgets/csirtg-mail-py",
     license='MPL2',
     classifiers=[
-               "Topic :: System :: Networking",
-               "Environment :: Other Environment",
-               "Intended Audience :: Developers",
-               "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-               "Programming Language :: Python",
-               ],
+        "Topic :: System :: Networking",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python",
+    ],
     keywords=['security'],
     author="Wes Young",
     author_email="wes@csirtgadgets.org",
     packages=find_packages(),
-    install_requires=[
-        'beautifulsoup4',
-        'pyzmail',
-        'chardet',
-        'lxml',
-        'html5lib'
-    ],
+    install_requires=get_requirements_by_version(),
     entry_points={
         'console_scripts': cmds
     },
